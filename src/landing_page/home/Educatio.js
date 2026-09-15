21fed465c11a53e52ddf59b553cdf00f292129d6
Akhilesh Rajbhar <rajbharakhilesh125@gmail.com>
2026-09-15 18:55:25 +0530

    Set up Bootstrap and Material Icons, create Home and Reward pages

---
+++ src/landing_page/home/Educatio.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Education = () => {
+  return (
+    <>
+        <h1>Education</h1>
+        <i class="fa-solid fa-plus"></i>
+        <i class="fa-solid fa-heart-circle-check"></i>
+    </>
+  );
+}
+
+export default Education;
